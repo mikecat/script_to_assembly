d1c4d5448fb37e1940410162cbd5cb2bdf8af953
MikeCAT <mikecat@users.noreply.github.com>
2017-09-13 21:09:25 +0900

    implement generateFunction() for LPC1114FN28

--- LPC1114FN28Generator.java
+++ LPC1114FN28Generator.java
@@ -77,5 +77,63 @@ public class LPC1114FN28Generator extends AssemblyGenerator {
 	}
 
 	private void generateFunction(Function func) {
+		// 自動変数のオフセットを計算する
+		currentFunctionVariableOffset = new ArrayList<Integer>();
+		int variableNum = func.getVariableNumber();
+		int variableOffset = 0;
+		int argumentOffset = 24;
+		for (int i = 0; i < variableNum; i++) {
+			AutomaticVariable var = func.getVariable(i);
+			if (var.getId() != currentFunctionVariableOffset.size()) {
+				throw new SystemLimitException("automatic variable ID mismatch");
+			}
+			int size = var.getDataType().getWidth();
+			long alignedSizeLong = (((long)size + 3) / 4) * 4;
+			if (alignedSizeLong > Integer.MAX_VALUE) {
+				throw new SystemLimitException("variable size too big after alignment");
+			}
+			int alignedSize = (int)alignedSizeLong;
+			if (var.isArgument()) {
+				if ((long)argumentOffset + alignedSize > Integer.MAX_VALUE) {
+					throw new SystemLimitException("arugments too big");
+				}
+				// 追加した後で次の引数に備えて更新
+				currentFunctionVariableOffset.add(argumentOffset);
+				argumentOffset += alignedSize;
+			} else {
+				if ((long)variableOffset - alignedSize < Integer.MIN_VALUE) {
+					throw new SystemLimitException("local variables too big");
+				}
+				// 今の変数のために更新した後、追加
+				variableOffset -= alignedSize;
+				currentFunctionVariableOffset.add(variableOffset);
+			}
+		}
+		if (variableOffset > 508) {
+			throw new SystemLimitException("local variables too big to support in this version");
+		}
+		// 関数の開始
+		currentFunctionName = func.getName();
+		currentFunctionReturnType = func.getReturnType();
+		out.println(currentFunctionName + ":");
+		out.println("\tPUSH {R4-R7,LR}");
+		out.println("\tMOV R1, R12");
+		out.println("\tPUSH {R1}");
+		out.println("\tMOV R12, SP");
+		if (variableOffset != 0) out.println("\tSUB SP, #" + (-(long)variableOffset));
+		// 関数内の命令をそれぞれ出力する
+		int instNum = func.getInstructionNumber();
+		for (int i = 0; i < instNum; i++) {
+			generateInstruction(func.getInstruction(i));
+		}
+		// 関数の終わり
+		out.println("stoa.funcend." + currentFunctionName + ":");
+		out.println("\tMOV SP, R12");
+		out.println("\tPOP {R1}");
+		out.println("\tMOV R12, R1");
+		out.println("\tPOV {R4-R7,PC}");
+	}
+
+	private void generateInstruction(Instruction inst) {
 	}
 }
